d0202a904621d9e419db3871c509404085d154db
yanarn <usndlhdn@gmail.com>
2022-07-26 11:06:53 +0900

    入力フォームまでできました

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '走れ三日坊主'
+    return render_template("index.html")
+
+@app.route("/receive", methods=["post"])
+def post():
+    text = request.form["name"]
+    return render_template("receive.html", name = text)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
