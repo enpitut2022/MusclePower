7455f9b7de6e7c7958e02d6df422c1e24d42522d
ProfessorFjord4 <professorfjord4@gmail.com>
2022-07-27 15:21:24 +0900

    メンバーの名前の登録の際のapp.pyの挙動を記述しました。

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dorkfibkuvlyob:1db04b410515df5d6a68a6fb88ba5272d7e5c7f5951442b1997441b0b5ef2ab5@ec2-34-235-31-124.compute-1.amazonaws.com:5432/d8b4bie8clcibc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/muscle'
+#リモートの場合は 'postgresql://dorkfibkuvlyob:1db04b410515df5d6a68a6fb88ba5272d7e5c7f5951442b1997441b0b5ef2ab5@ec2-34-235-31-124.compute-1.amazonaws.com:5432/d8b4bie8clcibc'
+#ローカルの場合は 'postgresql://localhost/muscle'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -14,6 +16,11 @@ class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     description = db.Column(db.String(200))
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    teamid = db.Column(db.Integer)
 
 @app.route('/')
 def index():
@@ -24,6 +31,15 @@ def index():
 def detail(id):
     teamdata = Team.query.get(id)
     return render_template("detail.html", td=teamdata)
+
+@app.route('/join', methods=["post"])
+def join():
+    name = request.form["name"]
+    teamid = request.form["teamid"]
+    newMember = Member(name=name, teamid=teamid)
+    db.session.add(newMember)
+    db.session.commit()
+    return redirect("/detail/"+str(teamid))
 
 """
 不必要になったコード(念の為残しておく)
