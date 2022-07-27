94b2e0ce4474d502a7b56c77079eb860d543cd99
ProfessorFjord4 <professorfjord4@gmail.com>
2022-07-27 11:18:28 +0900

    データベースにapp.pyが接続できるようになりました。

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
 import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/muscle'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    description = db.Column(db.String(200))
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    teams = Team.query.all()
+    return render_template("index.html", teams = teams)
 
 @app.route('/walking')
 def walking():
@@ -27,4 +39,4 @@ def post():
     return render_template("receive.html", name = name, detail=detail, time=time)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
